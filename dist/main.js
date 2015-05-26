43e84113ad0ad1ebad0fbd29afda9a87b1388865
Joshua Lagrange <jlagrange87@gmail.com>
2015-05-26 12:48:45 -0500

    Updated main.js to include notes

--- dist/main.js
+++ dist/main.js
@@ -3,11 +3,11 @@ $(document).on("ready", start);
 function start(e){
 	var $btn = $("#likeButton");
 	$btn.on("click", like);
-	var count = 0
+	var count = 0 //Initialize a count from 0 in the beginning
 
 	function like(e){
-		count++
-		console.log(count);
+		count++ //Start the count
+		console.log(count); //I wanted to see if it output a number like I wanted it to.
 		if(count === 1) {
 			$btn.html("1 Like");
 		}	
